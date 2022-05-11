26a7952ca45d32995ec8f63b66d2f39c748f23c0
GomezSolim <x.wsolim@gmail.com>
2022-05-11 20:33:15 +0100

    fichier a jour

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 
 function verify(){
     var html='';
-    var $res = $(".text");
-    $res.empty();
-    var noms=document.getElementById('name').options[document.getElementById('name').selectedIndex].textContent;
+    
+/*    $res.empty();
+*/  var noms=document.getElementById('name').value;
     var emails=document.getElementById('email').value;
     var satiss=document.getElementById('satis').value;
     var coachs=document.getElementById('coach').value;
     var messages=document.getElementById('message').value;
-    const TextDesValeurSaisies=document.querySelector('.text');
-    TextDesValeurSaisies.innerText=noms+emails+satiss+coachs+messages
 
-                    html="<div class='col-md-12 text-center'>"
-                        html+="<div class='alert alert-success' role='alert'>"
-                            html+="<h4 class='alert-heading'>"+noms+"</h4>"
-                            html+="<p>"+emails+"</p>"
-                            html+="<p>"+satiss+"</p>"
-                            html+="<p>"+coachs+"</p>"
-                            html+="<p>"+messages+"</p>"
-                        html+="</div>"
-                    html+="</div>"
+    html="<div class='col-md-12 text-center'>";
+        html+="<div class='alert alert-success' role='alert'>";
+            html+="<h1 class='alert-heading'> NOM :"+noms+"</h1>";
+            html+="<p> EMAIL : "+emails+"</p>";
+            html+="<p> STATIS : "+satiss+"</p>";
+            html+="<p> COACH : "+coachs+"</p>";
+            html+="<p> MESSAGE : "+messages+"</p>";
+        html+="</div>";
+    html+="</div>";
 
-                        $(".text").append(html);
+    alert(html);
+    document.getElementById('info').innerHTML=html;
 
-    console.log(noms+emails+satiss+coachs+messages);
+/*        $(".info").append(html);
+*/
+        
+
 }
 
