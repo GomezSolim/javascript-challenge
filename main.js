2ce8da949581fd923c7cfc0ce9c6bc1956dfaeaa
iMac G1 <imacg1@iMac-de-Energy-25.local>
2022-05-12 09:51:37 +0000

    correction of errors found in the three files

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ function verify(){
     var html='';
     
 /*    $res.empty();
-*/  var noms=document.getElementById('name').value;
+*/  
+    var noms=document.getElementById('name').value;
     var emails=document.getElementById('email').value;
     var satiss=document.getElementById('satis').value;
     var coachs=document.getElementById('coach').value;
     var messages=document.getElementById('message').value;
 
-    html="<div class='col-md-12 text-center'>";
+    html="<div class= col-md-12 text-center'>";
         html+="<div class='alert alert-success' role='alert'>";
-            html+="<h1 class='alert-heading'> NOM :"+noms+"</h1>";
-            html+="<p> EMAIL : "+emails+"</p>";
-            html+="<p> STATIS : "+satiss+"</p>";
-            html+="<p> COACH : "+coachs+"</p>";
-            html+="<p> MESSAGE : "+messages+"</p>";
+            html+="<h2 class='alert-heading'>"+noms+"</h2>";
+            html+="<p> "+emails+"</p>";
+            html+="<p>"+satiss+"</p>";
+            html+="<p>"+coachs+"</p>";
+            html+="<p>"+messages+"</p>";
         html+="</div>";
     html+="</div>";
 
